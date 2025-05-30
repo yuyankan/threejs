1297bb750f6696d9b45bfe3a14e3c7529b0e3c6a
yuyankan <89712442+yuyankan@users.noreply.github.com>
2025-05-30 08:42:49 +0800

    Create part_meta_info.js

---
+++ three_js_template/part_meta_info.js
@@ -0,0 +1,23 @@
+export const parts2read = {
+    "roller_d":0x00008b,
+    "roller_silicone":0x1155ccff,
+    "oven_silicone":0x00ffff,
+    "roller_glue":0x1155ccff,
+    "oven_glue":0x00ffff,
+    "roller_merge":0x1155ccff,
+    "roller_f":0x00008b,
+    "roller_finish":0x00008b,
+}
+
+export const initial_position = {
+    "roller_d":[-6,0],
+    "roller_silicone":[-9,0],
+    "oven_silicone":[-7,2],
+    "roller_glue":[-2,0],
+    "oven_glue":[0.5,2],
+    "roller_merge":[6.2,0],
+    "roller_f":[3,0],
+    "roller_finish":[10,0],
+}
+
+
